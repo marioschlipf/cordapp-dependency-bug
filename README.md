704e0fc90424925da4a806e959bd487df82704e5
Mario Schlipf <mario.schlipf@googlemail.com>
2018-07-06 13:31:10 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cordapp-dependency-bug
+Forked cordapp-example [https://github.com/corda/cordapp-example] to show a bug when including subprojects with
+```compile project('...')```
+
+The resulting jar will then include corda jars etc. that should not be there.
---
+++ subproject/src/HelloContract.kt
@@ -0,0 +1,13 @@
+package com.example.contract
+
+import com.example.state.IOUState
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.requireSingleCommand
+import net.corda.core.contracts.requireThat
+import net.corda.core.transactions.LedgerTransaction
+
+open class HelloContract : Contract {
+    override fun verify(tx: LedgerTransaction) {
+    }
+}
